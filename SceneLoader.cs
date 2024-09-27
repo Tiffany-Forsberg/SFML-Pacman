cda0f512e1432cbd930696b787df9db70bc822d0
Tore Forsberg <erotfor@gmail.com>
2024-09-27 14:00:47 +0200

    Add SceneLoader class
    Co-Authored-By: Rickardssson <180270630+rickardssson@users.noreply.github.com>

---
+++ SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using SFML.Graphics;
+using System.Collections.Generic;
+
+namespace Pacman
+{
+    class SceneLoader
+    {
+        private readonly Dictionary<char, Func<Entity>> loaders;
+        private string currentScene = "", nextScene = "";
+
+        public SceneLoader()
+        {
+            
+        }
+
+        private bool Create(char symbol, out Entity created)
+        {
+            if (loaders.TryGetValue(symbol, out Func<Entity> loader))
+            {
+                created = loader();
+                return true;
+            }
+
+            created = null;
+            return false;
+        }
+    }
+}
