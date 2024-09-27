8837cd4b35f3c30d81a043cc45185a392cc29178
Tore Forsberg <erotfor@gmail.com>
2024-09-27 13:20:55 +0200

    Initialize project
    Co-Authored-By: Rickardssson <180270630+rickardssson@users.noreply.github.com>

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System;
+
+namespace Pacman { 
+    class Program { 
+        static void Main(string[] args) {
+            using (
+                var window = new RenderWindow(new VideoMode(828, 900), "Pacman")
+            ) {
+                window.Closed += (o, e) => window.Close();
+                
+                // Initialize
+                Clock clock = new Clock();
+                
+                while (window.IsOpen) {
+                    window.DispatchEvents();
+                    float deltaTime = clock.Restart().AsSeconds();
+                    deltaTime = MathF.Min(deltaTime, 0.01f);
+                    
+                    // Updates
+                    window.Clear(new Color(223, 246, 245));
+                    
+                    // Drawing
+                    window.Display();
+                }
+            }
+        }
+    }
+}
