3cfbc7d54990532d4a7cfffa23af94c628ace61c
Tore Forsberg <erotfor@gmail.com>
2024-09-27 15:00:31 +0200

    fix wall texture
    Co-Authored-By: Rickardssson <180270630+rickardssson@users.noreply.github.com>

--- Wall.cs
+++ Wall.cs
@@ -12,7 +12,7 @@ namespace Pacman
         public override void Create(Scene scene)
         {
             base.Create(scene);
-            sprite.TextureRect = new IntRect(0, 0, 18, 18);
+            sprite.TextureRect = new IntRect(54, 54, 18, 18);
         }
         
         public override void Update(Scene scene, float deltaTime) {}
