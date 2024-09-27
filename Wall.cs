a785a198d8d5fabb277556f153517785a9a0e455
Tore Forsberg <erotfor@gmail.com>
2024-09-27 14:11:36 +0200

    Add wall class
    Co-Authored-By: Rickardssson <180270630+rickardssson@users.noreply.github.com>

---
+++ Wall.cs
@@ -0,0 +1,20 @@
+﻿using SFML.Graphics;
+using SFML.System;
+
+namespace Pacman
+{
+    public sealed class Wall : Entity
+    {
+        public Wall() : base("pacman") {}
+
+        public override bool Solid => true;
+
+        public override void Create(Scene scene)
+        {
+            base.Create(scene);
+            sprite.TextureRect = new IntRect(0, 0, 18, 18);
+        }
+        
+        public override void Update(Scene scene, float deltaTime) {}
+    }
+}
